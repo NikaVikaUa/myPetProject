0aed6fe089c2d60de25144d34d6529fffb1f4c6c
punisher <semendiaievaviktoriia@gmail.com>
2024-01-03 18:01:44 +0100

    start1

---
+++ src/features/games/types/Game.ts
@@ -0,0 +1,14 @@
+export default interface Game {
+	id: number;
+	image: string;
+	title: string;
+	price: number;
+	genre: string;
+	state: string;
+	session: string;
+	numberOfPlayers: string;
+	minAge: string;
+	description: string;
+	address: string;
+	releaseDate: Date;
+}
